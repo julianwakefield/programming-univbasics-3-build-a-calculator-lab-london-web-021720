680820678fadc0b4262dd485b008f73d8c339ae2
Julian Wakefield <julianwakefield@outlook.com>
2020-02-10 15:14:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,21 +8,37 @@ return sum
 end
 
 def subtraction(num1, num2)
-
+  num1=10
+  num2=5
+  sum = num1-num2
+  puts "sum"
+  return sum
 end
 
 def division(num1, num2)
-
+  num1=50
+  num2=2
+  sum = num1/num2
+  puts "sum"
+  return sum
 end
 
 def multiplication(num1, num2)
-
+  num1=4
+  num2=30
+  sum = num1*num2
+  puts "sum"
+  return sum
 end
 
 def modulo(num1, num2)
-
+  sum = num1+num2
+  puts "sum"
+  return sum
 end
 
 def square_root(num)
-
+  sum = num1+num2
+  puts "sum"
+  return sum
 end
