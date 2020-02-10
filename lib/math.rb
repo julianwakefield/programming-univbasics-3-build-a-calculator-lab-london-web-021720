cbdf029674f1ba2bd6b18ed0ddac88469c27477a
Julian Wakefield <julianwakefield@outlook.com>
2020-02-10 15:17:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -32,13 +32,13 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  sum = num1+num2
+  num1=34
+  num2-5
+  sum = num1/num2
   puts "sum"
   return sum
 end
 
 def square_root(num)
-  sum = num1+num2
-  puts "sum"
-  return sum
+
 end
